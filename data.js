9aaf44f10877417582556e73512050a2530d63fa
Icar038 <167262697+Icar038@users.noreply.github.com>
2025-05-26 11:43:56 -0300

    Update data.js

--- data.js
+++ data.js
@@ -18,7 +18,7 @@ const data = [
     itemCategory: "vida de rua",
     itemLink: "https://twitter.com/codegridweb",
     itemCopy:
-      "Fashion takes a digital leap with Integrated Electronic Wearables. It's not just about wearing technology—it's about integrating it seamlessly into everyday style. From garments that light up with embedded LEDs to outfits that can display your current mood through colors and patterns, the future of fashion is connected, smart, and always evolving.",
+      "Desde que chegou às ruas, Sandro, que ganhou o apelido de "Mancha" por conta de marcas que possuía na pele, acabou se viciando em cola de sapateiro — também conhecida apenas como "cola". Para sustentar seu vício, cometia pequenos assaltos e furtos pelas ruas do Rio de Janeiro, e por isso acabou sendo preso inúmeras vezes. Ainda menor de idade, Sandro ficou internado no Instituto Padre Severino, um reformatório público localizado na Ilha do Governador. Em muitas das instituições por onde passou, ele era considerado um jovem tranquilo. Segundo documentos da época, no Instituto Padre Severino ele recebia visitas de sua tia materna, de sua irmã de 14 anos e de alguns primos. Quando já quase adulto, foi adotado por uma mulher chamada Elza, que acreditava ser sua mãe biológica. No entanto, após a realização de um exame de DNA, descobriu-se que ela não era, de fato, sua mãe verdadeira.",
      media: [
       { type: "image", src: "./img-12-situacao-familiar.jpg" },
       { type: "image", src: "./img-13-parecer-social.jpg" },
